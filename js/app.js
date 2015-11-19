4e62bf8eb2768472666db0c723857de792497116
Scott Christensen <scottmc526@gmail.com>
2015-11-19 13:35:37 -0700

    updated css, calc wont allow letter submissions

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@
 
 var button = document.getElementById('paper');
 button.addEventListener('click', function calctip ()
-  { var dollars = document.getElementById('money').value.replace('$',"");
+  {
+  var dollars = document.getElementById('money').value.replace('$',"");
   var tip = dollars * .2;
   var fixed = tip.toFixed(2);
   var content = document.getElementById('total').innerHTML = "You should tip $" + fixed;
